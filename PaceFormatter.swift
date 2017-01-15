b920792d213ab2b7391f58caa956698b7eee2a55
Victor Hudson <vichudson1@gmail.com>
2017-01-15 10:34:17 -0500

    * Made formatter and WalkingPace type alias Public * Added an internal private method for time units conversion of the WalkingPace to remove dependency on the TimeInterval extension. * Updated documentation.

--- PaceFormatter.swift
+++ PaceFormatter.swift
@@ -5,19 +5,25 @@
 import Foundation
 
 /// Type alias to give the formatter some context on its input.
-typealias WalkingPace = Double
+public typealias WalkingPace = Double
 
 /// For converting `WalkingPace` instances to a string
-struct PaceFormatter {
+public struct PaceFormatter {
 	
-	/// Use to convert `WalkingPace` instances to a string.
-	///
+	/// Converts `WalkingPace` instances to a string.
+	
+	/// - Example: 2 miles walked over 43 minutes, 16 seconds would be calculated as 2596 seconds / 2 miles = 1298 walking pace, which would be formatted as `21'38"` for 21 minutes, 38 seconds per mile.
 	/// - Parameter pace: The walking pace value.
-	/// - Returns: string representing the walking pace in the form `19'23"`.
-	/// - Note: Depends on the `timeUnits` extension on `TimeInterval`.
+	/// - Returns: string representing the walking pace.
+	/// - precondition: Needs a `WalkingPace` value > 1 to work properly. Calculate pace in the form of `seconds/km` or `seconds/mi` to generate a high enough value for your `WalkingPace`.
 	func string(from pace: WalkingPace) -> String {
-		let units = pace.timeUnits
+		let units = timeUnits(for: pace)
 		return "\(units.minutes)'\(units.seconds)\""
+	}
+	
+	private func timeUnits(for pace: WalkingPace) -> (hours: Int, minutes: Int, seconds: Int) {
+		let interval = Int(pace)
+		return (interval / 3600, (interval % 3600) / 60, (interval % 3600) % 60)
 	}
 }
 
